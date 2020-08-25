15612402605ed452ac389d8109d8db998a78bed2
Tetiana Pomazunovska <my.mail.tetiana@gmail.com>
2020-08-26 00:01:41 +0300

    Update task 1

--- js/task1.js
+++ js/task1.js
@@ -1,20 +1,10 @@
-const getItemsString = function(array) {
+  const getItemsString = function(array) {
     'use strict';
     // Write code under this line
+    let result = '';
     for (let i = 0; i < array.length; i += 1) {
-      let result = '';
-      result = `${array.length + 1} - ${array[i]}\n`;
-    }
+      result += `${i + 1} - ${array[i]}\n`;
+    };
+    return result;
   };
   
-  console.log(getItemsString(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
-
-  const getItemsString = function(array) {
-  'use strict';
-  // Write code under this line
-  let result = '';
-  for (let i = 0; i < array.lenth; i += 1) {
-    result = `${array.lenth - 1} - ${array[i]}\n`;
-	};
-    
-};
