f6c1e369966a342f4c4e983de2565b18dd5aa325
Tetiana Pomazunovska <my.mail.tetiana@gmail.com>
2020-09-01 12:59:26 +0300

    Добавляет дз 2

--- js/task3.js
+++ js/task3.js
@@ -3,22 +3,22 @@
 
 function findLongestWord (string = "") {
     // Write code under this line
-    let mas = string.split(' ');
-    let longestWord = mas[0];
-    for (const str of mas){
-        
-        if (str.length < longestWord.length) {
-            longestWord = str;
+    let words = string.split(' ');
+    let longestWord = words[0];
+
+    for (const word of words){
+        if (word.length > longestWord.length) {
+            longestWord = word;
         }
     }
     return longestWord;
-  }
+}
   
-  console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
-  // 'jumped'
+// console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+// 'jumped'
   
-  //console.log(findLongestWord('Google do a roll'));
-  // 'Google'
+//console.log(findLongestWord('Google do a roll'));
+// 'Google'
   
-  //console.log(findLongestWord('May the force be with you'));
-  // 'force'
+//console.log(findLongestWord('May the force be with you'));
+// 'force'
